0a1341a6484d9c3b6e4578416f6ea4cd5a1a4ad9
AquariusPower <AquariusPower@users.noreply.github.com>
2018-05-28 18:35:40 -0300

    fix gitNewBranchFromUser.sh to let project be configurable

--- .devsPrefs/AquariusPower/git tools/gitNewBranchFromUser.sh
+++ .devsPrefs/AquariusPower/git tools/gitNewBranchFromUser.sh
@@ -4,6 +4,7 @@ strHelp="create a new branch using another user's branch of that project"
 source "`which gitToolsCommonCode.sh`"
 
 strUser="$1";shift #help <strUser> user name on github
+strProject="$1";shift #help <strProject> the project id/name
 strBranch="$1";shift #help <strBranch> user branch on github
 : ${strBaseBranch:=master};shift #help [strBaseBranch] base local branch to start from
 
@@ -11,4 +12,4 @@ FUNCexecEchoW git branch
 
 FUNCexecEchoW git checkout -b "${strUser}-${strBranch}" master
 
-FUNCexecEcho git pull "https://github.com/$strUser/ivan.git" "${strBranch}"
+FUNCexecEcho git pull "https://github.com/$strUser/${strProject}.git" "${strBranch}"
